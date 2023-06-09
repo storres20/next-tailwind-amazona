669a7cb5f16990d5011c5b8a6e5ebaa038dffca9
Italo Lon kan <sanchotorres777@gmail.com>
2023-06-09 12:38:27 -0500

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
+import ProductItem from '@/components/ProductItem'
+import data from '../utils/data'
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">next tailwind amazona</h1>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem
+            key={product.slug}
+            product={product}
+          />
+        ))}
+      </div>
     </div>
   )
 }
 
-/* 
+/*
 Nextjs ECommerce Tutorial For Beginners 2023 [Next.js, MongoDB & Tailwind]
 channel: Coding with Basir
 https://www.youtube.com/watch?v=_IBlyR5mRzA&list=WL&index=16&t=1694s

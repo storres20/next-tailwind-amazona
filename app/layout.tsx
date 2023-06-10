5cdc4465c41d6418552742a55c863f428a16a48b
Italo Lon kan <sanchotorres777@gmail.com>
2023-06-10 11:19:31 -0500

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Store, StoreProvider } from '@/utils/Store'
+import { useContext } from 'react'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,37 +20,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex min-h-screen flex-col justify-between">
-          <header>
-            <nav className="flex h-12 items-center px-4 justify-between shadow-md">
-              <Link
-                href="/"
-                className="text-lg font-bold"
-              >
-                Amazona
-              </Link>
-              <div>
-                <Link
-                  href="/cart"
-                  className="p-2"
-                >
-                  Cart
-                </Link>
-                <Link
-                  href="/login"
-                  className="p-2"
-                >
-                  Login
-                </Link>
-              </div>
-            </nav>
-          </header>
-          <main className="container m-auto mt-4 px-4">{children}</main>
-          <footer className="flex h-10 justify-center items-center shadow-inner">
-            <p>Copyright 2023 Amazona</p>
-          </footer>
-        </div>
+        <StoreProvider>
+          <RootContent>{children}</RootContent>
+        </StoreProvider>
       </body>
     </html>
+  )
+}
+
+function RootContent({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen flex-col justify-between">
+      <Header />
+      <main className="container m-auto mt-4 px-4">{children}</main>
+      <footer className="flex h-10 justify-center items-center shadow-inner">
+        <p>Copyright 2023 Amazona</p>
+      </footer>
+    </div>
   )
 }
